ba8275962da5a203bb2f6ed685370cacc1f0f147
Abhi727464 <abhaysinghparmar123@gmail.com>
2023-02-13 18:17:05 +0530

    animation

--- src/Components/HandpickedSpices/HandPicked.jsx
+++ src/Components/HandpickedSpices/HandPicked.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import mixSpices from "../../../src/assets/mixspices.png";
 import "./handpicked.css";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const HandPicked = () => {
+  useEffect(() => {
+    AOS.init();
+  }, [])
   return (
     <div className="mix-spices flex flex-col items-center justify-center relative">
+      <div
+      data-aos="fade-up" 
+      data-aos-duration="1000"
+      // data-aos-anchor-placement="top-center"
+    >
+    
       <div className="mix-spices-img">
         <img src={mixSpices} alt="" />
+      </div>
       </div>
       <div className="handpicked absolute top-[40%] bg-center bg-cover">
         <div className="para-container mt-20 lg:ml-10">
